c684c37cdea6cf8c8251ef048be4815f46af2b53
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 04:34:04 +0530

    :sparkles: Add package

--- mod.ts
+++ mod.ts
@@ -1,4 +1,14 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
+/** Check if a website is up */
+export async function isUp(url: string): Promise<boolean> {
+  const hostname = encodeURIComponent((new URL(url)).hostname);
+  const result: {
+    "domain": string;
+    "port": number;
+    "status_code": 1 | 2 | 3;
+    "response_ip": string;
+    "response_code": number;
+    "response_time": number;
+  } = await (await fetch(`https://isitup.org/${hostname}.json`))
+    .json();
+  return result.status_code === 1;
 }
